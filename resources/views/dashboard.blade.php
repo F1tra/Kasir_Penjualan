0f5712b9b5441dcc046a21a0f8dcd156903e05e2
F1tra <fitramaulana53@gmail.com>
2023-01-20 03:47:52 +0700

    menambhakan informasi master barang pada dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -37,6 +37,23 @@ Dashboard
             </div>
         </div>
 
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1"><a
+                                    href="{{ url('master-barang') }}" class="stretched-link">Barang</a></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $mbarang }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-boxes fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 <!-- /.container-fluid -->
